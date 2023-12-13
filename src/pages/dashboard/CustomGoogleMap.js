d8e679e09d64a68ef75e0a324b8c0fd3827d52c5
emrebuyukegribey <e.b.egribey@gmail.com>
2023-12-13 10:37:41 +0300

    edited dashboard

--- src/pages/dashboard/CustomGoogleMap.js
+++ src/pages/dashboard/CustomGoogleMap.js
@@ -1,48 +1,58 @@
-import React from 'react'
+import React from "react";
 
 class CustomGoogleMap extends React.PureComponent {
-    render() {
-        const { compose, withProps, lifecycle } = require("recompose");
-        const {
-            withScriptjs,
-            withGoogleMap,
-            GoogleMap,
-        } = require("react-google-maps");
+  render() {
+    const { compose, withProps, lifecycle } = require("recompose");
+    const {
+      withScriptjs,
+      withGoogleMap,
+      GoogleMap,
+    } = require("react-google-maps");
 
-
-        const CustomGoogleMap = compose(
-            withProps({
-                googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAW-dGjfYKIR5DGK93BvBXekHTsGr5197g",
-                loadingElement: <div style={{ height: `100%` }} />,
-                containerElement: <div style={{ height: '90vh'}} />,
-                mapElement: <div style={{ height: `98%` }} />,
-            }),
-            lifecycle({
-                componentWillMount() {
-                    this.setState({
-                        latitude: 0,
-                        center: {
-                            lat:38.4237, lng:27.1428
-                        },
-                    })
-                },
-            }),
-            withScriptjs,
-            withGoogleMap
-        )(props =>
-            <GoogleMap
-                defaultZoom={10}
-                center={props.center}
-                onBoundsChanged={props.onBoundsChanged}
-                componentRestrictions={{country: 'IN'}}
-                defaultOptions={{mapTypeControl: false, scaleControl: true, streetViewControl: false,  fullscreenControl: false}}
-            >
-               
-            </GoogleMap>
-        );
-        return(
-            <CustomGoogleMap mapCallback={this.props.mapCallback}/>
-        )
-    }
+    const CustomGoogleMap = compose(
+      withProps({
+        googleMapURL:
+          "https://maps.googleapis.com/maps/api/js?key=AIzaSyAW-dGjfYKIR5DGK93BvBXekHTsGr5197g",
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: "85vh" }} />,
+        mapElement: (
+          <div
+            style={{
+              height: `98%`,
+              borderRadius: "10px",
+              border: "5px solid #fff",
+            }}
+          />
+        ),
+      }),
+      lifecycle({
+        componentWillMount() {
+          this.setState({
+            latitude: 0,
+            center: {
+              lat: 38.4237,
+              lng: 27.1428,
+            },
+          });
+        },
+      }),
+      withScriptjs,
+      withGoogleMap
+    )((props) => (
+      <GoogleMap
+        defaultZoom={10}
+        center={props.center}
+        onBoundsChanged={props.onBoundsChanged}
+        componentRestrictions={{ country: "IN" }}
+        defaultOptions={{
+          mapTypeControl: false,
+          scaleControl: true,
+          streetViewControl: false,
+          fullscreenControl: false,
+        }}
+      ></GoogleMap>
+    ));
+    return <CustomGoogleMap mapCallback={this.props.mapCallback} />;
+  }
 }
-export default CustomGoogleMap
+export default CustomGoogleMap;
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -13,29 +13,34 @@ import UserTable from "./UserTable";
 import { Helmet } from "react-helmet";
 import BreadcrumbItem from "antd/es/breadcrumb/BreadcrumbItem";
 import { useEffect } from "react";
-import { Tabs, Input, Row, Col } from 'antd';
-import { SearchOutlined, AppstoreAddOutlined, EnterOutlined, FilterOutlined } from '@ant-design/icons';
-import { PlusOutlined } from '@ant-design/icons';
-import { Button, Tooltip} from 'antd';
-import { Switch, Space } from 'antd';
+import { Tabs, Input, Row, Col } from "antd";
+import {
+  SearchOutlined,
+  AppstoreAddOutlined,
+  EnterOutlined,
+  FilterOutlined,
+} from "@ant-design/icons";
+import { PlusOutlined } from "@ant-design/icons";
+import { Button, Tooltip } from "antd";
+import { Switch, Space } from "antd";
 function Dashboard(props) {
   const { activeLeftBar } = useContext(MainContext);
   const items = [
     {
-      key: '1',
-      label: 'Harita',
+      key: "1",
+      label: "Harita",
       children: <MapBoard />,
     },
     {
-      key: '2',
-      label: 'Listeler',
-      children: 'Content of Tab Pane 2',
+      key: "2",
+      label: "Listeler",
+      children: "Content of Tab Pane 2",
     },
     {
-      key: '3',
-      label: 'Grafikler',
-      children: 'Content of Tab Pane 3',
-    }
+      key: "3",
+      label: "Grafikler",
+      children: "Content of Tab Pane 3",
+    },
   ];
   useEffect(() => {
     props.setNavbarHeaderText("Dashboard");
@@ -44,7 +49,6 @@ function Dashboard(props) {
   const onChange = (key) => {
     console.log(key);
   };
-
 
   return (
     <>
@@ -59,34 +63,76 @@ function Dashboard(props) {
       >
         <div className="dashboard-container">
           <div className="dashboard-toolbar">
-            <Row >
-              <Col span={8}><Input placeholder="Hızlı Arama" /></Col>
-              <Col span={16}>
-                <Button icon={<EnterOutlined />} style={{ marginLeft: '5px' }} />
-                <Button type="dashed" style={{ marginLeft: '5px' }} icon={<FilterOutlined />}>
-                  Arama Filitreleri
-                </Button>
-                <Button type="dashed" style={{ marginLeft: '5px' }} icon={<SearchOutlined />}>
-                  Adres Bul
-                </Button>
-                <Button type="dashed" style={{ marginLeft: '5px' }} icon={<AppstoreAddOutlined />}>
-                  Yeni Kayıt
-                </Button>
-                
-                <Switch style={{ marginLeft: '5px' }} checkedChildren="Mobil Kullanıcıları Gizle" unCheckedChildren="Mobil Kullanıcıları Göster"  />
-                <Switch style={{ marginLeft: '5px' }} checkedChildren="Süreçleri Gizle" unCheckedChildren="Süreçleri Göster"  />
-                <Switch style={{ marginLeft: '5px' }} checkedChildren="Paylaşımları Gizle" unCheckedChildren="Paylaşımları Göster"  />
-
-              </Col>
+            <Row>
+              <div
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Col span={6}>
+                  <Input placeholder="Hızlı Arama" />
+                </Col>
+                <Col span={10}>
+                  <Button
+                    icon={<EnterOutlined />}
+                    style={{ marginLeft: "5px" }}
+                  />
+                  <Button
+                    type="dashed"
+                    style={{ marginLeft: "5px" }}
+                    icon={<FilterOutlined />}
+                  >
+                    Arama Filitreleri
+                  </Button>
+                  <Button
+                    type="dashed"
+                    style={{ marginLeft: "5px" }}
+                    icon={<SearchOutlined />}
+                  >
+                    Adres Bul
+                  </Button>
+                  <Button
+                    type="dashed"
+                    style={{ marginLeft: "5px" }}
+                    icon={<AppstoreAddOutlined />}
+                  >
+                    Yeni Kayıt
+                  </Button>
+                </Col>
+                <Col
+                  span={8}
+                  style={{ display: "flex", justifyContent: "flex-end" }}
+                >
+                  <Switch
+                    style={{ marginLeft: "5px" }}
+                    checkedChildren="Mobil Kullanıcıları Gizle"
+                    unCheckedChildren="Mobil Kullanıcıları Göster"
+                  />
+                  <Switch
+                    style={{ marginLeft: "5px" }}
+                    checkedChildren="Süreçleri Gizle"
+                    unCheckedChildren="Süreçleri Göster"
+                  />
+                  <Switch
+                    style={{ marginLeft: "5px" }}
+                    checkedChildren="Paylaşımları Gizle"
+                    unCheckedChildren="Paylaşımları Göster"
+                  />
+                </Col>
+              </div>
             </Row>
           </div>
           <Tabs
             className="dashboard-tab-container"
             size="large"
-            tabPosition='right'
+            tabPosition="right"
             defaultActiveKey="1"
             items={items}
-            onChange={onChange} />
+            onChange={onChange}
+          />
           {/*
           <div className="dashboard-widget-container">
             <DailyRecords />
