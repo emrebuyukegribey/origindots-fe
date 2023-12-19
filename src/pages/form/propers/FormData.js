e8e583a2dd67938cfd9d6c28366ead65ecaeeaf0
emrebuyukegribey <e.b.egribey@gmail.com>
2023-12-19 19:10:37 +0300

    some form fixed

--- src/pages/form/propers/FormData.js
+++ src/pages/form/propers/FormData.js
@@ -1,38 +1,64 @@
-import { Form, Upload, message } from "antd";
+import { Button, Form, Upload, message } from "antd";
 import { HiOutlinePhoto } from "react-icons/hi2";
 import "./FormItem.css";
 import { useState } from "react";
 import { AiOutlineUpload, AiOutlineVideoCameraAdd } from "react-icons/ai";
-
-const getBase64 = (img, callback) => {
-  const reader = new FileReader();
-  reader.addEventListener("load", () => callback(reader.result));
-  reader.readAsDataURL(img);
-};
-const beforeUpload = (file) => {
-  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
-  if (!isJpgOrPng) {
-    message.error("You can only upload JPG/PNG file!");
-    return;
-  }
-  const isLt2M = file.size / 1024 / 1024 < 2;
-  if (!isLt2M) {
-    message.error("Image must smaller than 2MB!");
-  }
-  return isJpgOrPng && isLt2M;
-};
+import { MdClose } from "react-icons/md";
 
 function FormData({ proper }) {
-  const [imageUrl, setImageUrl] = useState();
+  const [fileUrl, setFileUrl] = useState();
+
+  const getBase64 = (img, callback) => {
+    console.log("img : ", img);
+    const reader = new FileReader();
+    reader.addEventListener("load", () => callback(reader.result));
+    reader.readdAsText(img);
+  };
+  const beforeUpload = (file) => {
+    const isAvailableFile =
+      file.type === "application/pdf" ||
+      file.type === "application/xls" ||
+      file.type === "application/xlsx";
+
+    console.log("isAvailableFile : ", isAvailableFile);
+    if (!isAvailableFile) {
+      message.error("You can only upload PDF/XLS/XLSX file!");
+      return;
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error("Image must smaller than 2MB!");
+    }
+    return isAvailableFile && isLt2M;
+  };
   const handleChange = (info) => {
+    console.log("info : ", info);
     if (info.file.status === "uploading") {
       return;
     }
     if (info.file.status === "done") {
       getBase64(info.file.originFileObj, (url) => {
-        setImageUrl(url);
+        console.log("url : ", url);
+        localStorage.setItem(proper.id, url);
+        setFileUrl(url);
       });
     }
+  };
+
+  const onRemove = () => {
+    setFileUrl(null);
+    localStorage.removeItem(proper.id);
+  };
+
+  const [files, setFiles] = useState([]);
+
+  const uploadChanged = (event) => {
+    setFiles(event.fileList);
+  };
+
+  const fileRemoved = (event) => {
+    const filteredFiles = files.filter((file) => file !== event);
+    setFiles(filteredFiles);
   };
 
   return (
@@ -47,19 +73,16 @@ function FormData({ proper }) {
         ]}
       >
         <Upload
-          maxCount={1}
-          listType="picture"
-          showUploadList={true}
-          beforeUpload={beforeUpload}
-          onChange={handleChange}
-          // action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
-          size="large"
+          name="file"
+          showUploadList={{ showRemoveIcon: true }}
+          accept=".xls, .xlsx, .pdf"
+          beforeUpload={() => false}
+          onChange={(e) => uploadChanged(e)}
+          onRemove={(e) => fileRemoved(e)}
         >
-          <div className="form-photo-container ">
-            <AiOutlineUpload className="photo-field-upload-icon" />
-            <div>{proper.placeholder}</div>
-          </div>
+          <Button icon={<MdClose />}>Click to Upload</Button>
         </Upload>
+        {fileUrl && <div>file</div>}
       </Form.Item>
     </>
   );
--- src/pages/form/propers/FormPhoto.js
+++ src/pages/form/propers/FormPhoto.js
@@ -2,38 +2,12 @@ import { Form, Upload, message } from "antd";
 import { HiOutlinePhoto } from "react-icons/hi2";
 import "./FormItem.css";
 import { useEffect, useState } from "react";
-
-const getBase64 = (img, callback) => {
-  const reader = new FileReader();
-  reader.addEventListener("load", () => callback(reader.result));
-  reader.readAsDataURL(img);
-};
-const beforeUpload = (file) => {
-  if (file) {
-    localStorage.setItem("file", JSON.stringify(file));
-  }
-
-  if (!file && localStorage.getItem("file")) {
-    file = JSON.parse(localStorage.getItem("file"));
-    console.log("file : ", file);
-  }
-
-  console.log("file : ", file);
-
-  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
-  if (!isJpgOrPng) {
-    message.error("You can only upload JPG/PNG file!");
-    return;
-  }
-  const isLt2M = file.size / 1024 / 1024 < 2;
-  if (!isLt2M) {
-    message.error("Image must smaller than 2MB!");
-  }
-  return isJpgOrPng && isLt2M;
-};
+import { MdClose } from "react-icons/md";
 
 function FormPhoto(props) {
   const [imageUrl, setImageUrl] = useState();
+  const [file, setFile] = useState();
+
   const handleChange = (info) => {
     if (info.file.status === "uploading") {
       return;
@@ -41,15 +15,43 @@ function FormPhoto(props) {
     if (info.file.status === "done") {
       getBase64(info.file.originFileObj, (url) => {
         setImageUrl(url);
-        localStorage.setItem("imageUrl", imageUrl);
+        localStorage.setItem(props.proper.id, url);
+        // localStorage.setItem("file", JSON.stringify(info.file));
+        setFile(info.file);
       });
     }
   };
 
-  useEffect(() => {
-    beforeUpload();
-  }, []);
+  const getBase64 = (img, callback) => {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => callback(reader.result));
+    reader.readAsDataURL(img);
+  };
 
+  const beforeUpload = (file) => {
+    const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+    if (!isJpgOrPng) {
+      message.error("You can only upload JPG/PNG file!");
+      return;
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error("Image must smaller than 2MB!");
+    }
+    return isJpgOrPng && isLt2M;
+  };
+
+  const onRemove = () => {
+    localStorage.removeItem(props.proper.id);
+    setFile(null);
+    setImageUrl(null);
+    console.log("onRemove ");
+  };
+
+  useEffect(() => {
+    setFile(JSON.parse(localStorage.getItem("file")));
+    setImageUrl(localStorage.getItem(props.proper.id));
+  }, []);
   return (
     <>
       <Form.Item
@@ -67,17 +69,26 @@ function FormPhoto(props) {
         <Upload
           maxCount={1}
           listType="picture"
-          showUploadList={true}
+          showUploadList={false}
           beforeUpload={beforeUpload}
           onChange={handleChange}
           action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
           size="large"
+          onRemove={onRemove}
         >
           <div className="form-photo-container ">
             <HiOutlinePhoto className="photo-field-upload-icon" />
             <div>{props.proper.placeholder}</div>
           </div>
         </Upload>
+        {imageUrl && (
+          <div className="form-photo-image-container">
+            <img src={imageUrl} className="form-photo-image" />
+            <div onClick={onRemove} style={{ cursor: "pointer" }}>
+              <MdClose className="form-photo-close-icon" />
+            </div>
+          </div>
+        )}
       </Form.Item>
     </>
   );
