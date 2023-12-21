fcc35223f29e4b6bdb732452143219580df0f790
thehocca <durkaya.kemal@icloud.com>
2023-12-21 21:30:39 +0300

    Update UserRoleComponent.js

--- src/pages/organization/UserRoleComponent.js
+++ src/pages/organization/UserRoleComponent.js
@@ -12,82 +12,85 @@ class UserRoleComponent extends Component {
             userPolicyArray: props.user.userPolicies != null ? props.user.userPolicies.split(',') : ''
         };
 
-        this.handleSwitchChange = this.handleSwitchChange.bind(this);
+        // this.handleSwitchChange = this.handleSwitchChange.bind(this);
     }
 
 
 
     handleSwitchChange(checked, policyName) {
-        this.setState(prevState => {
-            const newPolicyArray = [...prevState.userPolicyArray];
+        console.log(checked + "-" + policyName + "-" + this.props.user);
 
-            if (checked) {
-                // Add policyName to array if not already included
-                if (!newPolicyArray.includes(policyName)) {
-                    newPolicyArray.push(policyName);
-                }
-            } else {
-                // Remove policyName from array if included
-                const index = newPolicyArray.indexOf(policyName);
-                if (index > -1) {
-                    newPolicyArray.splice(index, 1);
-                }
+        const newPolicyArray = this.props.user.userPolicies ? this.props.user.userPolicies.split(',') : [];
+
+        if (checked) {
+            // Add policyName to array if not already included
+            if (!newPolicyArray.includes(policyName)) {
+                newPolicyArray.push(policyName);
             }
+        } else {
+            // Remove policyName from array if included
+            const index = newPolicyArray.indexOf(policyName);
+            if (index > -1) {
+                newPolicyArray.splice(index, 1);
+            }
+        }
 
-            this.props.user.userPolicies=newPolicyArray.join(', ');
+        this.props.user.userPolicies = newPolicyArray.join(', ');
 
-            return { userPolicyArray: newPolicyArray };
-        });
+        this.setState({ userPolicyArray: newPolicyArray })
 
-
-        
+        return this.state.userPolicyArray;
     }
 
-    
+
+    checkedPolicy(user, key) {
+        return user.userPolicies ? user.userPolicies.includes(key) : false
+    }
 
     render() {
-   
+
+
         return (
             <>
                 <Row style={{ width: "75%" }}>
-                <Col span={24}>
-                    <Row className="row-space" justify="left" align="top">
+                    <Col span={24}>
+                        <Row className="row-space" justify="left" align="top">
 
-                        <Col span={7}><h5>SÜREÇ YÖNETİMİ</h5></Col>
-                        <Col span={5}>
-                            <Switch checkedChildren=" İnceleyebilir" unCheckedChildren=" İnceleyebilir"  onChange={(checked) => this.handleSwitchChange(checked, 'processRead')}/>
-                        </Col>
-                        <Col span={5}>
-                            <Switch checkedChildren=" Değiştirebilir" unCheckedChildren=" Değiştirebilir" onChange={(checked) => this.handleSwitchChange(checked, 'processWrite')}/>
-                        </Col>
+                            <Col span={7}><h5>SÜREÇ YÖNETİMİ</h5></Col>
+                            <Col span={5}>
+                                <Switch checkedChildren=" İnceleyebilir" unCheckedChildren=" İnceleyebilir" checked={this.checkedPolicy(this.props.user, "processRead")} onClick={(checked) => this.handleSwitchChange(checked, 'processRead')} />
+                            </Col>
+                            <Col span={5}>
+                                <Switch checkedChildren=" Değiştirebilir" unCheckedChildren=" Değiştirebilir" checked={this.checkedPolicy(this.props.user, "processWrite")} onClick={(checked) => this.handleSwitchChange(checked, 'processWrite')} />
+                            </Col>
 
-                    </Row>
+                        </Row>
 
-                    <Row className="row-space" justify="left" align="top">
+                        <Row className="row-space" justify="left" align="top">
 
-                        <Col span={7}><h5>KULLANICI YÖNETİMİ</h5></Col>
-                        <Col span={5}>
-                            <Switch checkedChildren=" İnceleyebilir" unCheckedChildren=" İnceleyebilir" onChange={(checked) => this.handleSwitchChange(checked, 'userRead')}/>
-                        </Col>
-                        <Col span={5}>
-                            <Switch checkedChildren=" Değiştirebilir" unCheckedChildren=" Değiştirebilir" onChange={(checked) => this.handleSwitchChange(checked, 'userWrite')}/>
-                        </Col>
+                            <Col span={7}><h5>KULLANICI YÖNETİMİ</h5></Col>
+                            <Col span={5}>
+                                <Switch checkedChildren=" İnceleyebilir" unCheckedChildren=" İnceleyebilir" checked={this.checkedPolicy(this.props.user, "userRead")} onClick={(checked) => this.handleSwitchChange(checked, 'userRead')} />
+                            </Col>
+                            <Col span={5}>
+                                <Switch checkedChildren=" Değiştirebilir" unCheckedChildren=" Değiştirebilir" checked={this.checkedPolicy(this.props.user, "userWrite")} onClick={(checked) => this.handleSwitchChange(checked, 'userWrite')} />
+                            </Col>
 
-                    </Row>
+                        </Row>
 
-                    <Row className="row-space" justify="left" align="top">
+                        <Row className="row-space" justify="left" align="top">
 
-                        <Col span={7}><h5>ORGANİZASYON YÖNETİMİ</h5></Col>
-                        <Col span={5}>
-                            <Switch checkedChildren=" İnceleyebilir" unCheckedChildren=" İnceleyebilir" onChange={(checked) => this.handleSwitchChange(checked, 'organizationRead')}/>
-                        </Col>
-                        <Col span={5}>
-                            <Switch checkedChildren=" Değiştirebilir" unCheckedChildren=" Değiştirebilir" onChange={(checked) => this.handleSwitchChange(checked, 'organizationWrite')}/>
-                        </Col>
+                            <Col span={7}><h5>ORGANİZASYON YÖNETİMİ</h5></Col>
+                            <Col span={5}>
+                                <Switch checkedChildren=" İnceleyebilir" unCheckedChildren=" İnceleyebilir" checked={this.checkedPolicy(this.props.user, "organizationRead")} onClick={(checked) => this.handleSwitchChange(checked, 'organizationRead')} />
+                            </Col>
+                            <Col span={5}>
+                                <Switch checkedChildren=" Değiştirebilir" unCheckedChildren=" Değiştirebilir" checked={this.checkedPolicy(this.props.user, "organizationWrite")} onClick={(checked) => this.handleSwitchChange(checked, 'organizationWrite')} />
+                            </Col>
 
-                    </Row>
+                        </Row>
 
-                </Col>
+                    </Col>
                 </Row>
             </>
         );
